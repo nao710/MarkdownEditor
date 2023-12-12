3c60add3c379497786c4562a1c7af3201329e8ae
nao710 <naototanaoto@outlook.jp>
2023-12-12 22:42:46 +0900

    CSS updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Editor from "./Editor";
 import Parse from "./Parse";
+import "./App.css";
 
 const App = () => {
   const initalDoc = "# Hello World!\n";
   const [code, setCode] = useState(initalDoc);
   return (
     <>
-      <Editor Value={code} onChange={(value) => setCode(value)} />
-      <Parse Value={code} />
+      <div className="content">
+        <Editor Value={code} onChange={(value) => setCode(value)} />
+        <Parse Value={code} />
+      </div>
     </>
   );
 };
