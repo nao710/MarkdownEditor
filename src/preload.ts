16b0044c0e1a1772bce5d85ad673313b77dce9d2
nao710 <naototanaoto@outlook.jp>
2024-01-13 12:57:12 +0900

    add window control

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,12 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from "electron";
+contextBridge.exposeInMainWorld(
+  'WindowControl', {
+  close: () => ipcRenderer.invoke('close'),
+  min: () => ipcRenderer.invoke('min'),
+  max: () => ipcRenderer.invoke('max'),
+  restore: () => ipcRenderer.invoke('restore')
+}
+)
